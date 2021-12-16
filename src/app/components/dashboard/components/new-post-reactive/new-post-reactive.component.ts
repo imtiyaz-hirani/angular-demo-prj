f4c4630cb4e5c460d9e2be85522a40aac9e75d50
imtiyaz-hirani <i.hirani@techskillsit.com>
2021-12-16 18:57:21 +0530

    form-group reactive form

--- src/app/components/dashboard/components/new-post-reactive/new-post-reactive.component.ts
+++ src/app/components/dashboard/components/new-post-reactive/new-post-reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard-new-post-reactive',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewPostReactiveComponent implements OnInit {
 
+  userForm: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.userForm = new FormGroup({
+      title: new FormControl('',
+        [Validators.required, Validators.minLength(3), Validators.maxLength(15),
+        Validators.pattern(/[a-zA-Z0-9 ]+/) ]),
+      body: new FormControl('', Validators.required),
+      userId: new FormControl('', Validators.required)
+    }
+    );
   }
 
 }
