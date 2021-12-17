43d99f0d6f81684aa94c31fc5b7decf759528e0e
imtiyaz-hirani <i.hirani@techskillsit.com>
2021-12-17 19:09:01 +0530

    form added

--- src/app/components/dashboard/components/post-view/post-view.component.ts
+++ src/app/components/dashboard/components/post-view/post-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Comment } from 'src/app/model/comment.model';
 import { Post } from 'src/app/model/post.model';
@@ -14,20 +15,27 @@ export class PostViewComponent implements OnInit {
   postId: number;
   post: Post;
   comments: Comment[];
+  postForm: FormGroup;
+
   displayStyle = "none";
   constructor(private activatedRoute: ActivatedRoute,
     private _dashboardService : DashboardService) { }
 
   ngOnInit(): void {
+
+    this.postForm = new FormGroup({
+      title: new FormControl('',
+        [Validators.required, Validators.minLength(3), Validators.maxLength(15),
+        Validators.pattern(/^[a-zA-Z0-9 ]+$/) ]),
+      body: new FormControl('', Validators.required),
+    }
+    );
     this.comments = [];
     this.postId = this.activatedRoute.snapshot.params['id'];
     console.log("act route id: " , this.postId);
 
-    //from here we reach out to service, getch post by id,
-    //and attach it to post
-
     this._dashboardService.getPostById(this.postId).subscribe(
-      (data)=> { this.post = data;   },
+      (data)=> { this.post = data;},
       (error)=>{  console.log('error'); },
       ()=> { console.log('complete');  }
     ) ;
@@ -38,11 +46,23 @@ export class PostViewComponent implements OnInit {
         this.comments = data;
       });
   }
+    //from here we reach out to service, getch post by id,
+    //and attach it to post
+
+
 
   openPopup() {
     this.displayStyle = "block";
   }
   closePopup() {
     this.displayStyle = "none";
+  }
+
+  editPost(){
+    console.log('post edited');
+  }
+
+  onFormSubmit(){
+
   }
 }
